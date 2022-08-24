2b0ff90cb68700e96ef38321a1a5307021d3e69b
HansiZeng <jupyter@notebook-hansi.c.gcp-ushi-digital-ds-qa.internal>
2022-08-24 17:44:48 +0000

    add user info side to preprocess

--- kgc-dr/scripts/unified_kgc/batch_unified_comb_train.sh
+++ kgc-dr/scripts/unified_kgc/batch_unified_comb_train.sh
@@ -3,13 +3,14 @@
 t_a2sp_path="/home/jupyter/jointly_rec_and_search/datasets/unified_kgc/unified_train/a2sp.train.tsv"
 t_a2cp_path="/home/jupyter/jointly_rec_and_search/datasets/unified_kgc/unified_train/a2cp.train.tsv"
 #t_q2a_path="/home/jupyter/jointly_rec_and_search/datasets/unified_kgc/unified_train/q2a.train.tsv"
-t_q2a_path="/home/jupyter/jointly_rec_and_search/datasets/unified_kgc/unified_train/max2_qorient_q2p.train.tsv"
+#t_q2a_path="/home/jupyter/jointly_rec_and_search/datasets/unified_kgc/unified_train/max2_qorient_q2p.train.tsv"
+t_q2a_path="/home/jupyter/jointly_rec_and_search/datasets/unified_kgc/unified_train/complete_q2p.train.tsv"
 
 #all_paths=("${t_a2sp_path}-None-None" "None-${t_a2cp_path}-None" "None-None-${t_q2a_path}" "${t_a2sp_path}-${t_a2cp_path}-None" "${t_a2sp_path}-None-${t_q2a_path}" "None-${t_a2cp_path}-${t_q2a_path}" "${t_a2sp_path}-${t_a2cp_path}-${t_q2a_path}")
 #all_paths=("${t_a2sp_path}-${t_a2cp_path}-${t_q2a_path}" "${t_a2sp_path}-${t_a2cp_path}-None" "${t_a2sp_path}-None-${t_q2a_path}" "None-${t_a2cp_path}-${t_q2a_path}" )
 all_paths=("${t_a2sp_path}-${t_a2cp_path}-${t_q2a_path}")
 
-EPOCHS=24
+EPOCHS=16
 
 for paths in "${all_paths[@]}"
 do
