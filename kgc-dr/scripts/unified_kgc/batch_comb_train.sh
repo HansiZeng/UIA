49d699c34f7a6a328847a5ffb9c58ba0431e8b49
HansiZeng <jupyter@notebook-hansi.c.gcp-ushi-digital-ds-qa.internal>
2022-09-14 20:34:09 +0000

    amazon_esci

--- kgc-dr/scripts/unified_kgc/batch_comb_train.sh
+++ kgc-dr/scripts/unified_kgc/batch_comb_train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
-t_a2sp_path="/home/jupyter/unity_jointly_rec_and_search/datasets/unified_kgc/unified_train/a2sp.train.tsv"
-t_a2cp_path="/home/jupyter/unity_jointly_rec_and_search/datasets/unified_kgc/unified_train/a2cp.train.tsv"
-t_q2a_path="/home/jupyter/unity_jointly_rec_and_search/datasets/unified_kgc/unified_train/max2_qorient_q2p.train.tsv"
+t_a2sp_path="/home/jupyter/unity_jointly_rec_and_search/datasets/unified_user/zero_shot/unified_kgc_train/a2sp.train.tsv"
+t_a2cp_path="/home/jupyter/unity_jointly_rec_and_search/datasets/unified_user/zero_shot/unified_kgc_train/a2cp.train.tsv"
+t_q2a_path="/home/jupyter/unity_jointly_rec_and_search/datasets/unified_user/zero_shot/unified_kgc_train/max2_qorient_q2p.train.tsv"
 
 entites_path="/home/jupyter/unity_jointly_rec_and_search/datasets/unified_user/all_entities.tsv"
-experiment_folder="/home/jupyter/unity_jointly_rec_and_search/experiments/unified_kgc/"
+experiment_folder="/home/jupyter/unity_jointly_rec_and_search/experiments/zero_shot/unified_kgc/"
 
 #all_paths=("${t_a2sp_path}-None-None" "None-${t_a2cp_path}-None" "None-None-${t_q2a_path}" "${t_a2sp_path}-${t_a2cp_path}-None" "${t_a2sp_path}-None-${t_q2a_path}" "None-${t_a2cp_path}-${t_q2a_path}" "${t_a2sp_path}-${t_a2cp_path}-${t_q2a_path}")
 all_paths=("${t_a2sp_path}-${t_a2cp_path}-None" "${t_a2sp_path}-None-${t_q2a_path}" "None-${t_a2cp_path}-${t_q2a_path}" "${t_a2sp_path}-${t_a2cp_path}-${t_q2a_path}")
